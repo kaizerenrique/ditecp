078bff2a69eb581a81518837aba40e42fa484622
kaizerenrique <kayserenrique@gmail.com>
2022-08-28 00:33:28 -0400

    componente para tokens de api

---
+++ app/Http/Livewire/Panel/ControlTokenApi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Panel;
+
+use Livewire\Component;
+
+class ControlTokenApi extends Component
+{
+    public function render()
+    {
+        return view('livewire.panel.control-token-api');
+    }
+}
