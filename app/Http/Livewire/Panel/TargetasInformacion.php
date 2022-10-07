df9767053465458ebc4660941a3903071103d2c6
kaizerenrique <kayserenrique@gmail.com>
2022-10-07 09:00:31 -0400

    informacion de uso

--- app/Http/Livewire/Panel/TargetasInformacion.php
+++ app/Http/Livewire/Panel/TargetasInformacion.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\Panel;
 
 use Livewire\Component;
 use Kaizerenrique\Consultabcv\Consultabcv;
+use App\Models\Registro;
 
 class TargetasInformacion extends Component
 {
@@ -38,13 +39,23 @@ class TargetasInformacion extends Component
 
         $tokenslis2 = auth()->user()->tokens()->orderBy('last_used_at', 'asc')->paginate(3);
 
+        //envia los registros de los tokens 
+        $registros = Registro::where('user_id', auth()->user()->id)->latest()->take(5)->get();
+
+        //envia informacion de los tokens
+        $tokeninfo = auth()->user()->tokens();
+
+        //dd($tokeninfo);
+
         return view('livewire.panel.targetas-informacion',[
             'usd' => $usd,
             'tokensNumero' => $tokensNumero,
             'tokensTotales' => $tokensTotales,
             'tokensRestantes' => $tokensRestantes,
             'tokenslis' => $tokenslis,
-            'tokenslis2' => $tokenslis2,            
+            'tokenslis2' => $tokenslis2, 
+            'registros' => $registros, 
+            'tokeninfo' => $tokeninfo,          
         ]);
     }
 }
--- resources/views/livewire/panel/targetas-informacion.blade.php
+++ resources/views/livewire/panel/targetas-informacion.blade.php
@@ -46,7 +46,7 @@
     </div>
 </div>
 
-<div class="grid grid-cols-1 lg:grid-cols-2 p-4 gap-4">
+<div class="grid grid-cols-1 lg:grid-cols-3 p-4 gap-4">
     <!-- ultimas conexiones -->
     <div class="relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
         <div class="rounded-t mb-0 px-0 border-0">
@@ -124,6 +124,43 @@
                             </tr>                            
                         @endforeach                        
                       </tbody>
+                </table>                
+            </div>
+        </div>
+    </div>
+    <!-- /ultimos registros -->
+    <!-- ultimos registros -->
+    <div class="relative flex flex-col min-w-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
+        <div class="rounded-t mb-0 px-0 border-0">
+            <div class="flex flex-wrap items-center px-4 py-2">
+                <div class="relative w-full max-w-full flex-grow flex-1">
+                <h3 class="font-semibold text-base text-gray-900 dark:text-gray-50">Ultimas consultas</h3>
+                </div>
+            </div>
+            <div class="block w-full">
+                <table class="items-center w-full bg-transparent border-collapse">
+                    <thead>
+                        <tr>
+                            <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle 
+                            border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 
+                            border-r-0 whitespace-nowrap font-semibold text-left">Función</th>
+                            <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle 
+                            border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 
+                            border-r-0 whitespace-nowrap font-semibold text-left">Actividad</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($registros as $registro )
+                            <tr class="text-gray-700 dark:text-gray-100">                                                                
+                                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                                    {{$registro->operacion}}                                                                          
+                                </th> 
+                                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                                    {{ \Carbon\Carbon::parse($registro->created_at)->diffForHumans() }}                                                                        
+                                </th>                                   
+                            </tr>                            
+                        @endforeach                        
+                    </tbody>
                 </table>                
             </div>
         </div>
