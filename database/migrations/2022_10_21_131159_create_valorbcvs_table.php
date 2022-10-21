767430f1bb8ae159bb0280ddd78f89944088e2c6
kaizerenrique <kayserenrique@gmail.com>
2022-10-21 09:16:20 -0400

    Modelo y migracion de la tabla de valores del BCV

---
+++ database/migrations/2022_10_21_131159_create_valorbcvs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('valorbcvs', function (Blueprint $table) {
+            $table->id();
+            $table->string('moneda');
+            $table->decimal('valor', 12, 8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('valorbcvs');
+    }
+};
